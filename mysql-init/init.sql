a94a0e5f98cc85d6c06851849bbe178b344ff5ca
Marie-France Fritz <mariefrance.fritz@outlook.fr>
2025-05-15 16:44:28 +0200

    clean data

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -45,8 +45,8 @@ VALUES (
 (
   'Spaghetti Bolognaise',
   'Un grand classique de la cuisine italienne, avec une sauce tomate mijotée à la viande hachée.',
-  '- 200g de spaghetti\n- 300g de viande hachée (bœuf ou mélange)\n- 1 oignon\n- 2 gousses d\'ail\n- 400g de pulpe de tomate\n- 2 c.à.s d\'huile d\'olive\n- Sel, poivre, herbes de Provence',
-  '2. Cuire les spaghetti dans de l\'eau bouillante salée.\n2. Faire revenir l’oignon et l’ail émincés dans l’huile d’olive.\n3. Ajouter la viande hachée et faire cuire jusqu’à ce qu’elle soit bien dorée.\n4. Verser la pulpe de tomate, ajouter les herbes, saler, poivrer, et laisser mijoter 20 minutes.\n5. Mélanger les pâtes à la sauce, servir chaud.',
+  '- 200g de spaghetti\n- 300g de viande hachée (bœuf ou mélange)\n- 1 oignon\n- 2 gousses d’ail\n- 400g de pulpe de tomate\n- 2 c.à.s d’huile d’olive\n- Sel, poivre, herbes de Provence',
+  '2. Cuire les spaghetti dans de l’eau bouillante salée.\n2. Faire revenir l’oignon et l’ail émincés dans l’huile d’olive.\n3. Ajouter la viande hachée et faire cuire jusqu’à ce qu’elle soit bien dorée.\n4. Verser la pulpe de tomate, ajouter les herbes, saler, poivrer, et laisser mijoter 20 minutes.\n5. Mélanger les pâtes à la sauce, servir chaud.',
   'https://images.unsplash.com/photo-1603133872878-684f208fb84d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3',
   2 -- ID de l'utilisateur qui crée la recette
 );
